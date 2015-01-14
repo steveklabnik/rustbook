714175083588a7b55892db839e04336c40cd2f7d
Steve Klabnik <steve@steveklabnik.com>
2015-01-14 19:12:17 +0000

    rebuild pages at d455edd

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['rustbook'] = {"items":[[0,"","rustbook",""],[10,"fmt","alloc::boxed","",0],[10,"from_err","","",0],[10,"description","collections::string","",1],[10,"description","alloc::boxed","",0],[10,"detail","","",0],[10,"cause","","",0],[10,"from_err","std::io","",2],[10,"description","","",2],[10,"detail","","",2]],"paths":[[1,"Box"],[1,"String"],[1,"IoError"]]};
+initSearch(searchIndex);
