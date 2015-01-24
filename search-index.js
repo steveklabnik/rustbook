c6869c167bfe4406ccbe3557314f2e0fb9b4ab74
Steve Klabnik <steve@steveklabnik.com>
2015-01-24 07:34:13 +0000

    rebuild pages at 0d54c06

--- search-index.js
+++ search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['rustbook'] = {"items":[[0,"","rustbook",""],[10,"fmt","alloc::boxed","",0],[10,"from_err","","",0],[10,"description","collections::string","",1],[10,"description","alloc::boxed","",0],[10,"detail","","",0],[10,"cause","","",0],[10,"from_err","std::io","",2],[10,"description","","",2],[10,"detail","","",2]],"paths":[[1,"Box"],[1,"String"],[1,"IoError"]]};
+searchIndex['rustbook'] = {"items":[[0,"","rustbook",""],[11,"fmt","alloc::boxed","",0],[11,"from_err","","",0],[11,"description","collections::string","",1],[11,"description","alloc::boxed","",0],[11,"detail","","",0],[11,"cause","","",0],[11,"from_err","std::io","",2],[11,"description","","",2],[11,"detail","","",2]],"paths":[[3,"Box"],[3,"String"],[3,"IoError"]]};
 initSearch(searchIndex);
