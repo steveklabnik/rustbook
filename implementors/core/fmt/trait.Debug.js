7d3b486ab69f5ec7abdd8050d9d20d131c86b060
Steve Klabnik <steve@steveklabnik.com>
2015-02-02 08:35:28 +0000

    rebuild pages at d3b4557

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rustbook'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='http://doc.rust-lang.org/nightly/alloc/boxed/struct.Box.html' title='alloc::boxed::Box'>Box</a>&lt;Error + 'static&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- search-index.js
+++ search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['rustbook'] = {"items":[[0,"","rustbook",""],[11,"fmt","alloc::boxed","",0],[11,"from_err","","",0],[11,"description","collections::string","",1],[11,"description","alloc::boxed","",0],[11,"detail","","",0],[11,"cause","","",0],[11,"from_err","std::io","",2],[11,"description","","",2],[11,"detail","","",2]],"paths":[[3,"Box"],[3,"String"],[3,"IoError"]]};
+searchIndex['rustbook'] = {"items":[[0,"","rustbook",""],[11,"fmt","alloc::boxed","",0],[11,"from_err","","",0],[11,"description","collections::string","",1],[11,"description","alloc::boxed","",0],[11,"detail","","",0],[11,"cause","","",0],[11,"from_err","std::old_io","",2],[11,"description","","",2],[11,"detail","","",2]],"paths":[[3,"Box"],[3,"String"],[3,"IoError"]]};
 initSearch(searchIndex);
